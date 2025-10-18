56549711cfc0a923d86d1ead7c6aae7b631fad23
fonz beato <96067139+whatbeato@users.noreply.github.com>
2025-10-18 15:06:02 +0100

    commit 2

---
+++ main.js
@@ -0,0 +1,24 @@
+const input = document.getElementById('input');
+const audioCtx = new AudioContext();
+const gainNode = audioCtx.createGain();
+
+const oscillator = audioCtx.createOscillator();
+oscillator.connect(gainNode);
+gainNode.connect(audioCtx.destination);
+oscillator.type = "sine";
+
+oscillator.start();
+gainNode.gain.value = 0;
+
+function frequency(pitch) {
+    gainNode.gain.setValueAtTime(100, audioCtx.currentTime);
+    oscillator.frequency.setValueAtTime(pitch, audioCtx.currentTime);
+    gainNode.gain.setValueAtTime(0, audioCtx.currentTime + 1);
+}
+
+audioCtx.resume();
+gainNode.gain.value = 0;
+
+function handle() {
+    frequency(input.value);
+}
